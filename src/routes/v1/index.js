45dee5ff89ed323e53cfb5f9d669e0bb51459d23
Chandan Tiwari <chandan.kr.tiwarii@gmail.com>
2026-09-03 00:25:20 +0000

    added airport functionality

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
-const { InfoController } = require('../../controllers');
+const { InfoController, AirportController } = require('../../controllers');
 
 const airplaneRoutes = require('./airplane-router')
 
 const cityRoutes = require('./ city-routes')
 
+const airportRoutes = require('./airport-routes');
+
 const router = express.Router();
 
 router.use('/airplanes' , airplaneRoutes)
 router.use('/city',cityRoutes)
+router.use('/airports',airportRoutes)
 router.get('/info', InfoController.info);
+
 
 module.exports = router;
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,6 +1,6 @@
 const {StatusCodes} = require('http-status-codes');
 
-const { CityRepository } = require('../repositories');
+const  {CityRepository}  = require('../repositories');
 const AppError = require('../utils/errors/app-error');
 
 const cityRepository = new CityRepository();
